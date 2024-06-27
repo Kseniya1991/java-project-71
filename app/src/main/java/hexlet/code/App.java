b06bde17aced6827143a8c01fd750f228b66a81e
Kseniya Zakharova <2b4got10@mail.ru>
2024-06-27 22:36:41 +0500

    4th step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,26 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
 
 @Command(name = "project", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
 public class App implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String file1;
+
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath2", description = "path to second file")
+    private String file2;
+
+    @Option(
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
+    private String format;
 
     @Override
     public Integer call() throws Exception {
