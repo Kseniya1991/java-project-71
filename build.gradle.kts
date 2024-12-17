512e1ef4516a4bb928ee5ff097cb1839a31afc12
root <root@DESKTOP-I4B85IA.>
2024-12-17 16:46:11 +0500

    modified

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
+}
+
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
